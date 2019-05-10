cf3f29eb52b02264fde30e9040a6fd04c4792d12
Samstep.mac <Samstep2012@gmail.com>
2019-05-10 11:32:59 -0700

    Added startup script

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# ./message.sh
+# ./start.sh
 
 ## Temp/Dev Variables
  ENDPOINT=
@@ -7,12 +7,15 @@
  RCON_DIR=
  RCON_PASS=
  URL=
- #SERVERID=
-
+##Panel Controller Vars
+ SERVER_ID=
+ CLIENT_ID=
+ SECRET=
+ TOKEN=$(curl -d "client_id=$CLIENT_ID&client_secret=$SECRET" -X POST http://$ENDPOINT/oauth2/token/request?grant_type=client_credentials | jq -r .access_token)
 ## Init/Functions
 echo "Panel Control by Samstep."
 
-echo "Running $0"
+echo "Running $0..."
 
 ##RCON as function
 rcon() {
@@ -30,11 +33,13 @@ broadcast() {
   curl -X POST -d "{\"username\": \"Server\", \"avatar_url\": \"https://image.flaticon.com/icons/png/512/226/226777.png\", \"content\": \"$*\"}" ${URL}
   echo "Curl sucessful"
   cd $RCON_DIR
-  ./rcon-cli --host $ENDPOINT --port $PORT --password $RCON_PASS "tellraw @a [\"[Broadcast] t$*\"]"
+  ./rcon-cli --host $ENDPOINT --port $PORT --password $RCON_PASS "tellraw @a [\"[Broadcast] $*\"]"
     echo "Broadcasted -> $*"
 }
 
-broadcast "Example broadcast to the server and Discord!"
+discord "Server starting..."
+sleep 1
+curl -X GET 'Content-type: application/json' -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" http://$ENDPOINT/daemon/server/$SERVER_ID/start
 
 #End script
 exit
